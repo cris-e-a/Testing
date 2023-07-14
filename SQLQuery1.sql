e2e804e1a83064eda220fc765a694c6093c7c93c
cris-e-a <139589037+cris-e-a@users.noreply.github.com>
2023-07-14 23:15:31 +0300

    Create SQLQuery1.sql
    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,4 @@
+SELECT * FROM [Detalii Clienti]
+SELECT * FROM [Detalii Clienti] WHERE Oras IS NOT NULL
+SELECT * FROM [Detalii Clienti] WHERE NUME IS NULL
+SELECT * FROM [Detalii Clienti] WHERE NUME IS NOT NULL
