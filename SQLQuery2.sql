a33cacd663a49469014396fded3768378fe943e5
cris-e-a <139589037+cris-e-a@users.noreply.github.com>
2023-07-14 23:15:15 +0300

    Create SQLQuery2.sql
    first commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(IDTranzactie)as 'Tranzactii'
+From Tranzactie
+Where IDClient=1
+SELECT COUNT(IDTranzactie)as 'Tranzactii'
+From Tranzactie
+Where IDClient=1 AND IDProduse=123 
+SELECT COUNT(IDTranzactie) as 'Produse cumparate'
+From Tranzactie
+Where IDProduse=124 
