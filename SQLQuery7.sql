98442d90c70e22e42e88b25a0a28909609610987
cris-e-a <139589037+cris-e-a@users.noreply.github.com>
2023-07-14 23:15:56 +0300

    Create SQLQuery7.sql
    first commit

---
+++ SQLQuery7.sql
@@ -0,0 +1,20 @@
+SELECT*FROM Elevi
+select count(Nota) as' Calificativ'
+From Note
+Where Nota>5
+select count(Nota) as' Calificativ'
+From Note
+Where Nota>5 and ID_elev<5 And ID_prof<5
+Select count(Nota) 
+From Note
+Where Nota>7 
+Select count(Nota) 
+From Note
+Where Nota>7 AND ID_materie<2
+Select *from Profesori
+Select count(Prenume_p)
+from Profesori
+SELECT * FROM Note 
+WHERE Data=23.04
+
+
